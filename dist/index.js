b63e626b06cbe0327886c3a23753bbeeda7ba071
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-06-02 14:00:36 +0200

    Force update

--- dist/index.js
+++ dist/index.js
@@ -25,20 +25,19 @@ class ApiClient {
             };
         });
     }
-    createPost({ title, summary, content, tags, }) {
+    createPost({ title, summary, content, tags, files, }) {
         return __awaiter(this, void 0, void 0, function* () {
+            let formData = new FormData();
+            formData.append("title", title);
+            if (summary) {
+                formData.append("summary", summary);
+            }
+            formData.append("content", content);
+            tags === null || tags === void 0 ? void 0 : tags.forEach((tag) => formData.append("tags", String(tag)));
+            files === null || files === void 0 ? void 0 : files.forEach((file) => formData.append("files", file));
             let response = yield fetch(this.baseUrl + "/api/posts", {
                 method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    title,
-                    summary,
-                    content,
-                    tags,
-                }),
+                body: formData,
             });
             if (response.status != 200) {
                 return { success: false, status: response.status };
@@ -92,6 +91,62 @@ class ApiClient {
                     name,
                 }),
             });
+            if (response.status != 200) {
+                return { success: false, status: response.status };
+            }
+            return {
+                success: true,
+                data: yield response.json(),
+            };
+        });
+    }
+    getFiles() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(this.baseUrl + "/api/files");
+            if (response.status != 200) {
+                return { success: false, status: response.status };
+            }
+            return {
+                success: true,
+                data: yield response.json(),
+            };
+        });
+    }
+    createFile(file, name, post) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let formData = new FormData();
+            formData.append("file", file);
+            formData.append("name", name);
+            formData.append("post", String(post));
+            let response = yield fetch(this.baseUrl + "/api/files", {
+                method: "POST",
+                body: formData,
+            });
+            if (response.status != 200) {
+                return { success: false, status: response.status };
+            }
+            return {
+                success: true,
+                data: yield response.json(),
+            };
+        });
+    }
+    deleteFile(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let response = yield fetch(this.baseUrl + "/api/files/" + id.toString(), {
+                method: "DELETE",
+            });
+            if (response.status != 204) {
+                return { success: false, status: response.status };
+            }
+            return {
+                success: true,
+            };
+        });
+    }
+    getFile(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(this.baseUrl + "/api/files/" + id.toString());
             if (response.status != 200) {
                 return { success: false, status: response.status };
             }
